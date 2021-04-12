36ae6406ff0afb689123641ec9db52f7b243c404
aleandrorsanchez <aleeexr7@gmail.com>
2021-04-12 12:29:24 +0200

    postUser endpoint implementado

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -1,11 +1,31 @@
 const userController = {};
 const db = require('../database');
+const bcrypt = require('bcrypt');
 
 userController.getUsers = (req, res) => {
     db.query('SELECT * FROM test', function (err, rows, fields) {
         if (err) throw err;
         res.json(rows);
     })
+}
+
+userController.postUser = (req, res) => {
+    const BCRYPT_SALT_ROUNDS = 12;
+    const password = req.body.password;
+    bcrypt.hash(password, BCRYPT_SALT_ROUNDS)
+        .then(function(hash){
+            const user = req.body;
+            user.password = hash;
+            const query = 'INSERT INTO users SET ?';
+            db.query(query, user, function (err, rows, fields) {
+                if (err) throw err;
+                res.send('User created');
+            })
+        })
+        .catch(function(error){
+           console.log('Error saving user');
+           next();
+        });
 }
 
 module.exports = userController;
--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const userController = require('../controllers/user.controller');
 
 router.get('/', userController.getUsers);
+router.post('/', userController.postUser);
 
 module.exports = router;
