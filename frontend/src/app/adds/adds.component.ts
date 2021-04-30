3eeaa1e105300b042114376574d1625add3efb24
aleandrorsanchez <aleeexr7@gmail.com>
2021-04-30 11:42:38 +0200

    service called from fronted

--- frontend/src/app/adds/adds.component.ts
+++ frontend/src/app/adds/adds.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {ActivatedRoute} from "@angular/router";
 import {AddListing} from "../shared/addListing";
+import {AddService} from "../shared/add.service";
 
 @Component({
   selector: 'app-adds',
@@ -12,16 +13,16 @@ export class AddsComponent implements OnInit {
   type: number;
   adds: AddListing[];
 
-  constructor(private route: ActivatedRoute) {
+  constructor(private route: ActivatedRoute, private addService: AddService) {
     this.type = this.route.snapshot.params.type;
   }
 
   ngOnInit(): void {
-
-  }
-
-  contactUser() {
-
+    this.addService.findByType(this.type).subscribe(
+      response => {
+        console.log(response);
+      }
+    );
   }
 
   isAdoption() {
--- frontend/src/app/shared/add.service.ts
+++ frontend/src/app/shared/add.service.ts
@@ -8,14 +8,20 @@ import {AddCreation} from "./addCreation";
 export class AddService {
 
   URL = 'http://localhost:3000/adds';
+  ADOPTION = '/adoption';
+  VOLUNTEER = '/volunteer';
 
   constructor(private http: HttpClient) { }
 
   saveAdoptionAdd(add: AddCreation) {
-    return this.http.post(this.URL + '/adoption', add, {observe: 'response'});
+    return this.http.post(this.URL + this.ADOPTION, add, {observe: 'response'});
   }
 
   saveVolunteerAdd(add: AddCreation) {
-    return this.http.post(this.URL + '/volunteer', add, {observe: 'response'});
+    return this.http.post(this.URL + this.VOLUNTEER, add, {observe: 'response'});
+  }
+
+  findByType(type: number) {
+    return this.http.get(this.URL + '/' + type);
   }
 }
