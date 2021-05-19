f7272e5d1e19fd3e6e378e77e73c9c01fa035114
aleandrorsanchez <aleeexr7@gmail.com>
2021-05-19 09:47:54 +0200

    email attr added to User

--- frontend/src/app/shared/models/user.ts
+++ frontend/src/app/shared/models/user.ts
@@ -2,12 +2,14 @@ export class User {
   id: number;
   username: string;
   password: string;
+  email: string;
   description: string;
   address: string;
 
   constructor() {
     this.username = '';
     this.password = '';
+    this.email = '';
     this.description = '';
     this.address = '';
   }
@@ -16,6 +18,7 @@ export class User {
     this.id = item.id;
     this.username = item.username;
     this.password = item.password;
+    this.email = item.email;
     this.description = item.description;
     this.address = item.address;
   }
